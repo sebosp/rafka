03e1161711db71d6d7657fdf3952f9393ff34b4f
Seb Ospina <kraige@gmail.com>
2020-12-25 12:42:53 +0100

    BrokerMetadataEndpoint reference to new mod, adding new method

--- rafka_core/src/server/kafka_server.rs
+++ rafka_core/src/server/kafka_server.rs
@@ -4,8 +4,10 @@
 //! - All fields that were initially null have been coverted to Option<T>, This is probably a bad
 //!   idea, let's see how far we can go
 
+use crate::server::broker_metadata_checkpoint::BrokerMetadataCheckpoint;
 use crate::server::broker_states::BrokerState;
 use crate::server::dynamic_config_manager::{ConfigEntityName, ConfigType};
+use crate::server::kafka_config::KafkaConfig;
 use crate::utils::kafka_scheduler::KafkaScheduler;
 use crate::zk::kafka_zk_client::KafkaZkClient;
 use crate::zookeeper::zoo_keeper_client::ZKClientConfig;
@@ -31,7 +33,6 @@ pub struct GroupCoordinator;
 pub struct TransactionCoordinator;
 pub struct KafkaController;
 pub struct MetadataCache;
-pub struct BrokerMetadataCheckpoint;
 struct BrokerTopicStats;
 struct FinalizedFeatureChangeListener;
 pub struct KafkaServer {
@@ -150,6 +151,24 @@ impl Default for KafkaServer {
 }
 
 impl KafkaServer {
+    /// `new` creates a new instance, expects a parsed config
+    pub fn new(config: KafkaConfig, time: std::time::Instant) -> Self {
+        let mut kafka_server = KafkaServer::default();
+        // TODO: In the future we can implement SSL/etc.
+        kafka_server.zk_client_config = ZKClientConfig::default();
+        let broker_meta_props_file = String::from("meta.properties");
+        // Using File.separator as "." since this is going to just work on Linux.
+        for bmc_log_dir in config.log_dirs {
+            let filename = format!("{}.{}", bmc_log_dir, broker_meta_props_file);
+            kafka_server
+                .broker_metadata_checkpoints
+                .insert(filename.clone(), BrokerMetadataCheckpoint::new(&filename));
+        }
+        kafka_server.init_time = time;
+        kafka_server
+    }
+
+    /// Main entry point for this class
     pub fn startup(&mut self) {
         info!("Starting");
         // These series of if might be pointless and we might get away by using mpsc from a
@@ -164,6 +183,7 @@ impl KafkaServer {
         let can_startup = self.is_starting_up.compare_and_swap(false, true, Ordering::Relaxed);
         if can_startup {
             self.broker_state = BrokerState::Starting;
+            self.init_zk_client();
         }
     }
 
