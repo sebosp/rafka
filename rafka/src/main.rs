742932df5330de087146ab3469e93f3375f095e3
Seb Ospina <kraige@gmail.com>
2021-01-12 19:04:59 +0100

    Starting to use tokio, testing multi_thread

--- rafka/src/main.rs
+++ rafka/src/main.rs
@@ -2,10 +2,13 @@ use clap::{App, Arg};
 use rafka_core::server::kafka_config::KafkaConfig;
 use rafka_core::server::kafka_server::KafkaServer;
 use std::time::Instant;
+use tokio::sync::mpsc;
 use tracing::Level;
+use tracing::{error, info};
 use tracing_subscriber::FmtSubscriber;
 
-fn main() {
+#[tokio::main(flavor = "multi_thread", worker_threads = 10)]
+async fn main() {
     let matches = App::new("Rafka")
         .version("0.0")
         .author("Seb Ospina <kraige@gmail.com>")
@@ -35,7 +38,15 @@ fn main() {
 
     let config_file = matches.value_of("INPUT").unwrap();
     println!("Using input file: {}", config_file);
+    let (main_tx, main_rx) = mpsc::channel(4_096); // TODO: Magic number removal
     let kafka_config = KafkaConfig::get_kafka_config(config_file).unwrap();
-    let mut kafka_server = KafkaServer::new(kafka_config, Instant::now());
-    kafka_server.startup();
+    let kafka_config_tmp = kafka_config.clone();
+    tokio::spawn(async move {
+        let mut kafka_server = KafkaServer::new(kafka_config_tmp, Instant::now(), main_tx);
+        match kafka_server.startup().await {
+            Ok(()) => info!("Kafka startup Complete"),
+            Err(err) => error!("Kafka startup failed: {:?}", err),
+        };
+    });
+    rafka_core::tokio::async_coordinator(kafka_config, main_rx).await;
 }
