25d8bc6befa02751774c96baf1594ef8028dc3a4
Seb Ospina <kraige@gmail.com>
2021-01-14 08:36:20 +0100

    Adding error variant for tokio mpsc, adding shutdown message

--- rafka_core/src/tokio/mod.rs
+++ rafka_core/src/tokio/mod.rs
@@ -1,3 +1,5 @@
+use crate::server::kafka_config::KafkaConfig;
+use crate::zk::kafka_zk_client::KafkaZkClient;
 use crate::zookeeper::zoo_keeper_client;
 use thiserror::Error;
 use tokio::sync::mpsc;
@@ -14,16 +16,24 @@ pub enum ZookeeperAsyncTask {
     EnsurePersistentPathExists(String),
 }
 
+#[derive(Debug)]
+pub enum CoordinatorTask {
+    Shutdown,
+}
+
 /// `AsyncTask` contains message types that async_coordinator can work on
 #[derive(Debug)]
 pub enum AsyncTask {
     Zookeeper(ZookeeperAsyncTask),
+    Coordinator(CoordinatorTask),
 }
 
 #[derive(Error, Debug)]
 pub enum AsyncTaskError {
     #[error("ZookeeperError {0:?}")]
-    ZooKeeperError(#[from] crate::zookeeper::zoo_keeper_client::ZooKeeperClientError),
+    ZooKeeperError(#[from] zookeeper_async::ZkError),
+    #[error("Mpsc SendError {0:?}")]
+    MpscSendError(#[from] tokio::sync::mpsc::error::SendError<AsyncTask>),
 }
 
 // pub async make_sure_persistent_path_exists(String) -> Result<(), AsyncTaskError> {
@@ -32,15 +42,21 @@ pub enum AsyncTaskError {
 // }
 
 #[instrument]
-pub async fn async_coordinator(mut rx: mpsc::Receiver<AsyncTask>) {
-    let mut zookeeper_client = zoo_keeper_client::ZooKeeperClient::default();
-    zookeeper_client.connect().await.unwrap();
+pub async fn async_coordinator(kafka_config: KafkaConfig, mut rx: mpsc::Receiver<AsyncTask>) {
     debug!("async_coordinator: Starting");
+    debug!("Connecting to ZooKeeper");
+    let mut kafka_zk_client = KafkaZkClient::default();
+    kafka_zk_client.create_chroot_path_if_set(&kafka_config.zk_connect);
+    kafka_zk_client.connect().await.unwrap();
     while let Some(message) = rx.recv().await {
         debug!("async_coordinator: message: {:?}", message);
         match message {
             AsyncTask::Zookeeper(zk_task) => info!("coordinator zk_task is {:?}", zk_task),
+            AsyncTask::Coordinator(coord_task) => {
+                info!("coordinator zk_task is {:?}", coord_task);
+                break;
+            },
         }
     }
-    error!("async_coordinator: Exiting. This shouldn't happen");
+    error!("async_coordinator: Exiting.");
 }
