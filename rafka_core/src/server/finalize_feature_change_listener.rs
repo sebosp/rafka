af2bea2369b8364ab042c0bbfe466682dcaae5a9
Seb Ospina <kraige@gmail.com>
2021-02-09 18:12:03 +0100

    Starting to add tx channel to talk to async_coordinator

--- rafka_core/src/server/finalize_feature_change_listener.rs
+++ rafka_core/src/server/finalize_feature_change_listener.rs
@@ -4,5 +4,9 @@
 //! is received from ZK, the feature cache in FinalizedFeatureCache is asynchronously updated
 //! to the latest features read from ZK. The cache updates are serialized through a single
 //! notification processor thread.
+use crate::tokio::{AsyncTask, AsyncTaskError, CoordinatorTask, ZookeeperAsyncTask};
+use tokio::sync::mpsc::{channel, Sender};
 #[derive(Debug)]
-pub struct FinalizedFeatureChangeListener;
+pub struct FinalizedFeatureChangeListener {
+    pub async_task_tx: Sender<AsyncTask>,
+}
