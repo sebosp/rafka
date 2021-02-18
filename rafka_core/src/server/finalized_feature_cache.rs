45e83f51d6ebd57fa3e4f6b7984244587ada9eaa
Seb Ospina <kraige@gmail.com>
2021-02-18 19:54:51 +0100

    Wrong struct for Display impl

--- rafka_core/src/server/finalized_feature_cache.rs
+++ rafka_core/src/server/finalized_feature_cache.rs
@@ -2,23 +2,20 @@
 //
 // Helper class that represents finalized features along with an epoch value.
 use std::fmt;
-#[Derive(Debug)]
+#[derive(Debug)]
 pub struct FinalizedFeaturesAndEpoch {
     features: Features<FinalizedVersionRange>,
     epoch: i32,
 }
 
-impl struct FinalizedFeaturesAndEpoch {
+impl FinalizedFeaturesAndEpoch {
     pub fn new(features: Features<FinalizedVersionRange>, epoch: i32) -> Self {
-        Self {
-            features,
-            epoch
-        }
+        Self { features, epoch }
     }
 }
 
-impl fmt::Display for Point {
+impl fmt::Display for FinalizedFeaturesAndEpoch {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "FinalizedFeaturesAndEpoch(features={}, epoch={})", features, epoch)
+        write!(f, "FinalizedFeaturesAndEpoch(features={}, epoch={})", self.features, self.epoch)
     }
 }
