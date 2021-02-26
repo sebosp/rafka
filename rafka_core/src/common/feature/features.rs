3ed89f278159e127b25dd836b68e0278b13695fa
Seb Ospina <kraige@gmail.com>
2021-02-26 19:55:13 +0100

    Trying without a Trait to avoid leaking it through the libraries

--- rafka_core/src/common/feature/features.rs
+++ rafka_core/src/common/feature/features.rs
@@ -8,88 +8,49 @@ use std::fmt;
 use thiserror::Error;
 use tracing::debug;
 
-pub trait VersionRangeType {
-    /// Attempts to create an instance from a HashMap, panics if the min/max labels do not exist as
-    /// keys in the HashMap
-    fn from_map(version_range_map: HashMap<String, i16>) -> Self {
-        debug!("Attempting to build from HashMap: {:?}", version_range_map);
-        match Self::try_from_map(version_range_map) {
-            Ok(val) => val,
-            Err(err) => panic!(err),
-        }
-    }
-
-    /// Attempts to create an instance from a HashMap, returns error if keys do not exist
-    fn try_from_map(version_range_map: HashMap<String, i16>)
-        -> Result<Self, BaseVersionRangeError>;
-
-    fn min(&self) -> i16 {
-        self.version_range.min()
-    }
-
-    fn max(&self) -> i16 {
-        self.version_range.max()
-    }
+#[derive(Debug)]
+pub enum VersionRangeType {
+    Supported(HashMap<String, SupportedVersionRange>),
+    Finalized(HashMap<String, FinalizedVersionRange>),
 }
 
 /// Represents an immutable dictionary with key being feature name, and value being
-/// something that implements VersionRangeType
+/// either Supported or Finalized VersionRanges
 #[derive(Debug)]
-pub struct Features<T>
-where
-    T: VersionRangeType,
-{
-    features: HashMap<String, T>,
+pub struct Features {
+    features: VersionRangeType,
 }
 
-#[derive(Error)]
+#[derive(Debug, Error)]
 pub enum FeaturesError {
     #[error("Provided features can not be empty")]
     EmptyVersionRangeType,
 }
 
 impl Features {
-    /// Attempts to create a new instance of Features
-    fn new<T>(features: HashMap<String, T>) -> Result<Self, FeaturesError>
-    where
-        T: VersionRangeType,
-    {
-        if features.is_empty() {
-            Err(FeaturesError::EmptyVersionRangeType)
-        } else {
-            Self { features }
-        }
-    }
-
     /// Creates a Features struct of type SupportedVersionRange
     pub fn supported_features(
         features: HashMap<String, SupportedVersionRange>,
-    ) -> Self<SupportedVersionRange> {
-        Self::new(features);
+    ) -> Result<Self, FeaturesError> {
+        Self::new(VersionRangeType::Supported(features))
     }
 
     /// Creates a Features struct of type FinalizedVersionRange
-    pub fn finalized_features(
-        features: HashMap<String, FinalizedVersionRange>,
-    ) -> Self<FinalizedVersionRange> {
-        Self::new(features);
+    pub fn finalized_features(features: HashMap<String, FinalizedVersionRange>) -> Self {
+        Self::new(VersionRangeType::Finalized(features))
     }
 
     pub fn is_empty(&self) {
-        self.features.isEmpty();
+        self.features.0.is_empty();
     }
 
-    /// Returns the Some(VersionRangeType) corresponding to the feature name, or None if the feature
-    /// is absent
-    pub fn get<T>(&self, feature: &str) -> Option<T>
-    where
-        T: VersionRangeType,
-    {
+    /// Looks up an item from the features HashMap and returns an Optional found VersionRangeType
+    pub fn get(&self, feature: &str) -> Option<VersionRangeType> {
         self.get(feature)
     }
 }
 
-impl fmt::Display for Features {
+impl<T> fmt::Display for Features<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
