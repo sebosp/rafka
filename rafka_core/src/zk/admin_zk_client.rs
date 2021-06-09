52cfabe8cb36ab69152f42a0b9c17c93768eb563
Seb Ospina <kraige@gmail.com>
2021-06-09 20:04:53 +0200

    Modifying AdminZkClient to use the new coordinators

--- rafka_core/src/zk/admin_zk_client.rs
+++ rafka_core/src/zk/admin_zk_client.rs
@@ -6,8 +6,28 @@
 //! This is an internal class and no compatibility guarantees are provided,
 //! see org.apache.kafka.clients.admin.AdminClient for publicly supported APIs.
 
+use crate::majordomo::AsyncTask;
 use crate::zk::kafka_zk_client::KafkaZkClient;
-#[derive(Debug, Default)]
+use crate::zk::zk_data::ZkData;
+use tokio::sync::mpsc;
+
+#[derive(Debug)]
 pub struct AdminZkClient {
-    zk_client: KafkaZkClient,
+    tx: mpsc::Sender<AsyncTask>,
+    zk_data: ZkData,
+}
+
+impl AdminZkClient {
+    pub fn new(tx: mpsc::Sender<AsyncTask>, zk_data: ZkData) -> Self {
+        Self { tx, zk_data }
+    }
+
+    pub async fn fetch_entity_config(
+        &mut self,
+        root_entity_type: String,
+        sanitized_entity_name: String,
+    ) -> String {
+        // XXX: This returns Properties
+        KafkaZkClient::getEntityConfigs(tx, root_entity_type, sanitized_entity_name).await?
+    }
 }
