315223a2accc2f0082f33b7ed894076bef674169
Seb Ospina <kraige@gmail.com>
2021-06-11 17:17:27 +0200

    Starting to think how to use the DynamicBrockerConfig

--- rafka_core/src/server/dynamic_broker_config.rs
+++ rafka_core/src/server/dynamic_broker_config.rs
@@ -1,8 +1,8 @@
 /// Dynamic Broker Configurations
 /// core/src/main/scala/kafka/server/DynamicBrokerConfig.scala
 /// Dynamic broker configurations are stored in ZooKeeper and may be defined at two levels:
-/// - Per-broker configs persisted at `/configs/brokers/{brokerId}`: These can be described/altered
-///   using AdminClient using the resource name brokerId.
+/// - Per-broker configs persisted at `/configs/brokers/{brokerId}`: These can be
+///   described/altered using AdminClient using the resource name brokerId.
 /// - Cluster-wide defaults persisted at `/configs/brokers/<default>`: These can be
 ///   described/altered using AdminClient using an empty resource name.
 /// The order of precedence for broker configs is:
@@ -15,18 +15,50 @@
 /// order of precedence above. Topic config overrides may use a different config name from the
 /// default broker config. See [[kafka.log.LogConfig#TopicConfigSynonyms]] for the mapping.
 ///
-/// AdminClient returns all config synonyms in the order of precedence when configs are described
-/// with `includeSynonyms`. In addition to configs that may be defined with the same name at
-/// different levels, some configs have additional synonyms.
-/// - Listener configs may be defined using the prefix `listener.name.{listenerName}.{configName}`.
-///   These may be configured as dynamic or static broker configs. Listener configs have higher
-///   precedence than the base configs that don't specify the listener name. Listeners without a
-///   listener config use the base config. Base configs may be defined only as STATIC_BROKER_CONFIG
-///   or DEFAULT_CONFIG and cannot be updated dynamically.
+/// AdminClient returns all config synonyms in the order of precedence when configs are
+/// described with `includeSynonyms`. In addition to configs that may be defined with the same
+/// name at different levels, some configs have additional synonyms.
+/// - Listener configs may be defined using the prefix
+///   `listener.name.{listenerName}.{configName}`. These may be configured as dynamic or static
+///   broker configs. Listener configs have higher precedence than the base configs that don't
+///   specify the listener name. Listeners without a listener config use the base config. Base
+///   configs may be defined only as STATIC_BROKER_CONFIG or DEFAULT_CONFIG and cannot be
+///   updated dynamically.
 /// - Some configs may be defined using multiple properties. For example, `log.roll.ms` and
-///   `log.roll.hours` refer to the same config that may be defined in milliseconds or hours. The
-///   order of precedence of these synonyms is described in the docs of these configs in
+///   `log.roll.hours` refer to the same config that may be defined in milliseconds or hours.
+///   The order of precedence of these synonyms is described in the docs of these configs in
 ///   [[kafka.server.KafkaConfig]].
+use crate::majordomo::{AsyncTask, AsyncTaskError};
+use crate::server::dynamic_config_manager::ConfigEntityName;
+use crate::server::kafka_config::KafkaConfig;
+use crate::zk::admin_zk_client::AdminZkClient;
+use crate::zk::zk_data::ZkData;
+use tokio::sync::mpsc;
 
-#[derive(Debug)]
-pub struct DynamicBrokerConfig;
+#[derive(Debug, PartialEq, Clone)]
+pub struct DynamicBrokerConfig {
+    pub kafka_config: KafkaConfig,
+}
+
+impl DynamicBrokerConfig {
+    fn initialize(kafka_config: KafkaConfig, tx: mpsc::Sender<AsyncTask>) -> Self {
+        let admin_zk_client = AdminZkClient::new(tx.clone());
+        let zk_data = ZkData::default();
+        let mut res = DynamicBrokerConfig { kafka_config };
+        let entity_conf_kv = admin_zk_client
+            .fetch_entity_config(zk_data.config_types.broker, ConfigEntityName::default().default);
+        res.update_default_config(entity_conf_kv.0, entity_conf_kv.1);
+        let prop_kv = admin_zk_client
+            .fetch_entity_config(zk_data.config_types.broker, kafka_config.broker_id.to_string());
+        res.update_broker_config(kafka_config.broker_id, prop_kv.0, prop_kv.1);
+        res
+    }
+
+    fn update_default_config(&mut self, key: String, value: String) {
+        unimplemented!()
+    }
+
+    fn update_broker_config(&mut self, broker_id: i32, key: String, value: String) {
+        unimplemented!()
+    }
+}
