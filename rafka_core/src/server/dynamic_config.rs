dea18df3f06b2646a7c94a3ebc210bc84f8cd1e0
Seb Ospina <kraige@gmail.com>
2021-07-06 20:41:17 +0200

    Renamed to ConfigDefs to avoid confusion with other structs

--- rafka_core/src/server/dynamic_config.rs
+++ rafka_core/src/server/dynamic_config.rs
@@ -5,43 +5,45 @@
 use crate::common::config_def::{ConfigDef, ConfigDefImportance};
 use crate::server::kafka_config::{KafkaConfigError, KafkaConfigProperties};
 use crate::server::replication_quota_manager::ReplicationQuotaManagerConfig;
+use std::collections::HashMap;
 
 pub const LEADER_REPLICATION_THROTTLED_RATE_PROP: &str = "leader.replication.throttled.rate";
 pub const FOLLOWER_REPLICATION_THROTTLED_RATE_PROP: &str = "follower.replication.throttled.rate";
 pub const REPLICA_ALTER_LOG_DIRS_IO_MAX_BYTES_PER_SECOND_PROP: &str =
     "replica.alter.log.dirs.io.max.bytes.per.second";
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct DynamicConfig {
-    broker: BrokerConfigs,
+    pub broker: BrokerConfigs,
 }
 
 #[derive(Debug)]
 pub struct BrokerConfigs {
-    dynamic_props: DynamicBrokerConfigs,
-    non_dynamic_configs: Vec<String>,
+    pub dynamic_props: DynamicBrokerConfigDefs,
+    pub non_dynamic_props: Vec<String>,
 }
 
 impl Default for BrokerConfigs {
     fn default() -> Self {
-        // Get the KafkaConfigProperties and remove the DynamicBrokerConfigs (in case they are by
+        // Get the KafkaConfigProperties and remove the DynamicBrokerConfigDefs (in case they are by
         // mistake maybe added?)
-        let mut configs = KafkaConfigProperties::config_names();
-        for dynamic_conf in DynamicBrokerConfigs::config_names() {
-            static_configs.drain_filter(|x| *x == dynamic_conf).collect::<Vec<_>>();
-        }
-        Self { dynamic_props: DynamicBrokerConfigs::default(), non_dynamic_configs }
+        let configs = KafkaConfigProperties::config_names();
+        let dynamic_conf = DynamicBrokerConfigDefs::config_names();
+        let (_, non_dynamic_props): (Vec<_>, Vec<_>) =
+            configs.into_iter().partition(|&e| dynamic_conf.contains(&e));
+
+        Self { dynamic_props: DynamicBrokerConfigDefs::default(), non_dynamic_props }
     }
 }
 
 #[derive(Debug)]
-pub struct DynamicBrokerConfigs {
+pub struct DynamicBrokerConfigDefs {
     leader_replication_throttled_rate_prop: ConfigDef<u64>,
     follower_replication_throttled_rate_prop: ConfigDef<u64>,
     replica_alter_log_dirs_io_max_bytes_per_second_prop: ConfigDef<u64>,
 }
 
-impl Default for DynamicBrokerConfigs {
+impl Default for DynamicBrokerConfigDefs {
     fn default() -> Self {
         Self {
             leader_replication_throttled_rate_prop: ConfigDef::default()
@@ -80,7 +82,7 @@ impl Default for DynamicBrokerConfigs {
     }
 }
 
-impl DynamicBrokerConfigs {
+impl DynamicBrokerConfigDefs {
     /// `try_from_config_property` transforms a string value from the config into our actual types
     pub fn try_set_property(
         &mut self,
@@ -106,9 +108,13 @@ impl DynamicBrokerConfigs {
     pub fn config_names() -> Vec<String> {
         // TODO: This should be derivable somehow too.
         vec![
-            LEADER_REPLICATION_THROTTLED_RATE_PROP,
-            FOLLOWER_REPLICATION_THROTTLED_RATE_PROP,
-            REPLICA_ALTER_LOG_DIRS_IO_MAX_BYTES_PER_SECOND_PROP,
+            LEADER_REPLICATION_THROTTLED_RATE_PROP.to_string(),
+            FOLLOWER_REPLICATION_THROTTLED_RATE_PROP.to_string(),
+            REPLICA_ALTER_LOG_DIRS_IO_MAX_BYTES_PER_SECOND_PROP.to_string(),
         ]
+    }
+
+    pub fn validate(props: HashMap<String, String>) -> Result<(), String> {
+        unimplemented!()
     }
 }
