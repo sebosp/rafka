bc3d28423edf597a4f3c53a2b64c9267d7296378
Seb Ospina <kraige@gmail.com>
2021-08-23 21:09:14 +0200

    Init cluster_resource

---
+++ rafka_core/src/common/cluster_resource.rs
@@ -0,0 +1,21 @@
+//! The ClusterResource metadata for a Kafka cluster.
+//! From clients/src/main/java/org/apache/kafka/common/ClusterResource.java
+
+use std::fmt::Display;
+#[derive(PartialEq, Debug, Hash)]
+pub struct ClusterResource {
+    pub cluster_id: Option<String>,
+}
+impl ClusterResource {
+    /// The cluster.id may be None (null on java), if the metadata request was sent to a cluster
+    /// prior to version 0.10.1.0.
+    pub fn new(cluster_id: Option<String>) -> Self {
+        Self { cluster_id }
+    }
+}
+
+impl Display for ClusterResource {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "klusterResource(clusterId={:?})", self.cluster_id)
+    }
+}
