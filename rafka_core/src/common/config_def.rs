3f580627c655d0f553ef80b4acdbd12abd3bd66b
Seb Ospina <kraige@gmail.com>
2021-09-19 12:38:21 +0200

    ConfigDef should have its validator function to avoid losing track of validating items

--- rafka_core/src/common/config_def.rs
+++ rafka_core/src/common/config_def.rs
@@ -1,13 +1,13 @@
-/// This class is used for specifying the set of expected configurations.
-/// From clients/src/main/java/org/apache/kafka/common/config/ConfigDef.java
+//! This class is used for specifying the set of expected configurations.
+//! From clients/src/main/java/org/apache/kafka/common/config/ConfigDef.java
 use crate::server::kafka_config::KafkaConfigError;
-use serde_derive::{Deserialize, Serialize};
+use std::fmt;
 use std::str::FromStr;
 use tracing::error;
 
 /// `ConfigDefImportance` provides the levels of importance that different java_properties
 /// have.
-#[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum ConfigDefImportance {
     High,
     Medium,
@@ -17,19 +17,53 @@ pub enum ConfigDefImportance {
 /// `ConfigDef` defines the configuration properties, how they can be resolved from other
 /// values and their defaults This should be later transformed into a derivable from something like
 /// DocOpt.
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct ConfigDef<T> {
-    importance: ConfigDefImportance,
-    doc: String,
     /// The configuration key that is used to apply this value
     pub key: String,
+    /// How important the configuration definition is
+    importance: ConfigDefImportance,
     /// `default` of the value, this would be parsed and transformed into each field type from
     /// KafkaConfig
     default: Option<String>,
+    /// The documentation of the field, used for showing errors
+    doc: String,
     /// Whether or not this variable was provided by the configuration file.
     provided: bool,
     /// The current value, be it the default or overwritten by config
     value: Option<T>,
+    /// A validator to ensure the new field value is correct
+    validator: Option<Box<dyn Fn(String) -> T>>,
+}
+
+impl<T> fmt::Debug for ConfigDef<T>
+where
+    T: fmt::Debug,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("ConfigDef")
+            .field("key", &self.key)
+            .field("importance", &self.importance)
+            .field("default", &self.default)
+            .field("doc", &self.doc)
+            .field("provided", &self.provided)
+            .field("value", &self.value)
+            .field("validator_exists", &self.validator.is_some())
+            .finish()
+    }
+}
+
+impl<T> PartialEq for ConfigDef<T>
+where
+    T: PartialEq,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.key == other.key
+            && self.importance == other.importance
+            && self.default == other.default
+            && self.doc == other.doc
+            && self.provided == other.provided
+            && self.value == other.value
+    }
 }
 
 impl<T> Default for ConfigDef<T> {
@@ -41,6 +75,7 @@ impl<T> Default for ConfigDef<T> {
             default: None,
             provided: false,
             value: None,
+            validator: None,
         }
     }
 }
@@ -74,11 +109,16 @@ where
         match default.parse::<T>() {
             Ok(val) => self.value = Some(val),
             Err(err) => {
-                error!("Unable to parse default property for {:?}: {}", self, err);
+                error!("Unable to parse default property for {:?}: {}", self.key, err);
                 panic!();
             },
         }
         self.default = Some(default);
+        self
+    }
+
+    pub fn with_validator(mut self, validator: Box<dyn Fn(String) -> T>) -> Self {
+        self.validator = Some(validator);
         self
     }
 
