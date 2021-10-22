77f0f74b4c07703e458c42be27f8131fa286c084
Seb Ospina <kraige@gmail.com>
2021-10-22 17:22:38 +0200

    Adidng the impl From KafkaConfig

--- rafka_core/src/log/cleaner_config.rs
+++ rafka_core/src/log/cleaner_config.rs
@@ -1,12 +1,14 @@
 //! From core/src/main/scala/kafka/log/CleanerConfig.scala
 //! Config parameters for the log cleaner
 
+use crate::server::kafka_config::KafkaConfig;
+
 #[derive(Debug)]
 pub struct CleanerConfig {
     /// The number of cleaner threads to run
     num_threads: i32,
     /// The total memory used for log deduplication
-    ded/upe_buffer_size: u64,
+    dedupe_buffer_size: u64,
     /// The maximum percent full for the deduplication buffer
     dedupe_buffer_load_factor: f64,
     io_buffer_size: i32,
@@ -34,6 +36,22 @@ impl Default for CleanerConfig {
             back_off_ms: 15 * 1000,
             enable_cleaner: true,
             hash_algorithm: String::from("MD5"),
+        }
+    }
+}
+
+impl From<&KafkaConfig> for CleanerConfig {
+    fn from(config: &KafkaConfig) -> Self {
+        CleanerConfig {
+            num_threads: config.log_cleaner_threads,
+            dedupe_buffer_size: config.log_cleaner_dedupe_buffer_size,
+            dedupe_buffer_load_factor: config.log_cleaner_dedupe_buffer_load_factor,
+            io_buffer_size: config.log_cleaner_io_buffer_size,
+            max_message_size: config.message_max_bytes,
+            max_io_bytes_per_second: config.log_cleaner_io_max_bytes_per_second,
+            back_off_ms: config.log_cleaner_backoff_ms,
+            enable_cleaner: config.log_cleaner_enable,
+            ..CleanerConfig::default()
         }
     }
 }
