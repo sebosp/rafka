2546247a1ee6d13a7915a4d2a7dbdd3a364c0ebf
Seb Ospina <kraige@gmail.com>
2022-03-04 21:09:46 +0100

    Continuing ConfigDef deriver

--- rafka_core/src/server/kafka_config/replication.rs
+++ rafka_core/src/server/kafka_config/replication.rs
@@ -2,8 +2,7 @@
 
 use super::{ConfigSet, KafkaConfigError, TrySetProperty};
 use crate::common::config_def::{ConfigDef, ConfigDefImportance};
-use enum_iterator::IntoEnumIterator;
-use std::fmt;
+use rafka_derive::ConfigDef;
 use std::str::FromStr;
 use tracing::trace;
 
@@ -15,72 +14,23 @@ pub const UNCLEAN_LEADER_ELECTION_ENABLE_DOC: &str =
     "Indicates whether to enable replicas not in the ISR set to be elected as leader as a last \
      resort, even though doing so may result in data loss";
 
-#[derive(Debug, IntoEnumIterator)]
-pub enum ReplicationConfigKey {
-    UncleanLeaderElectionEnable,
-}
-
-impl fmt::Display for ReplicationConfigKey {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::UncleanLeaderElectionEnable => {
-                write!(f, "{}", UNCLEAN_LEADER_ELECTION_ENABLE_PROP)
-            },
-        }
-    }
-}
-
-impl FromStr for ReplicationConfigKey {
-    type Err = KafkaConfigError;
-
-    fn from_str(input: &str) -> Result<Self, Self::Err> {
-        match input {
-            UNCLEAN_LEADER_ELECTION_ENABLE_PROP => Ok(Self::UncleanLeaderElectionEnable),
-            _ => Err(KafkaConfigError::UnknownKey(input.to_string())),
-        }
-    }
-}
-
-#[derive(Debug)]
+#[derive(Debug, ConfigDef)]
 pub struct ReplicationConfigProperties {
+    #[config_def(
+        key = UNCLEAN_LEADER_ELECTION_ENABLE_PROP,
+        importance = High,
+        doc = UNCLEAN_LEADER_ELECTION_ENABLE_DOC,
+        default = false,
+    )]
     pub unclean_leader_election_enable: ConfigDef<bool>,
 }
 
-impl Default for ReplicationConfigProperties {
-    fn default() -> Self {
-        Self {
-            unclean_leader_election_enable: ConfigDef::default()
-                .with_key(UNCLEAN_LEADER_ELECTION_ENABLE_PROP)
-                .with_importance(ConfigDefImportance::High)
-                .with_doc(UNCLEAN_LEADER_ELECTION_ENABLE_DOC)
-                .with_default(false),
-        }
-    }
-}
-
-impl TrySetProperty for ReplicationConfigProperties {
-    /// `try_from_config_property` transforms a string value from the config into our actual types
-    fn try_set_property(
-        &mut self,
-        property_name: &str,
-        property_value: &str,
-    ) -> Result<(), KafkaConfigError> {
-        let kafka_config_key = ReplicationConfigKey::from_str(property_name)?;
-        match kafka_config_key {
-            ReplicationConfigKey::UncleanLeaderElectionEnable => {
-                self.unclean_leader_election_enable.try_set_parsed_value(property_value)?
-            },
-        };
-        Ok(())
-    }
-}
 impl ConfigSet for ReplicationConfigProperties {
-    type ConfigKey = ReplicationConfigKey;
     type ConfigType = ReplicationConfig;
 
     fn resolve(&mut self) -> Result<Self::ConfigType, KafkaConfigError> {
         trace!("ReplicationConfigProperties::resolve()");
-        let unclean_leader_election_enable = self.unclean_leader_election_enable.build()?;
+        let unclean_leader_election_enable = self.build_unclean_leader_election_enable()?;
         Ok(Self::ConfigType { unclean_leader_election_enable })
     }
 }
