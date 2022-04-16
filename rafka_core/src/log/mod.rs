dbe2bd40ddd65dfcfaafdc5579d74c3e404fcc1a
Seb Ospina <kraige@gmail.com>
2022-04-16 16:41:05 +0200

    Init ProducerStateManager

--- rafka_core/src/log/mod.rs
+++ rafka_core/src/log/mod.rs
@@ -3,3 +3,4 @@ pub mod log;
 pub mod log_cleaner;
 pub mod log_config;
 pub mod log_manager;
+pub mod producer_state_manager;
---
+++ rafka_core/src/log/producer_state_manage.rs
@@ -0,0 +1,46 @@
+//! From core/src/main/scala/kafka/log/ProducerStateManager.scala
+
+// RAFKA TODO:
+#[derive(Clone, Hash, Ord)]
+pub struct ProducerStateEntry;
+
+// RAFKA TODO:
+#[derive(Clone, Hash, Ord)]
+pub struct TxnMetadata;
+
+pub struct ProducerStateManager {
+    topic_partition: TopicPartition,
+    log_dir: PathBuf,
+    max_producer_id_expiration_ms: i64,
+    log_ident: String,
+    producers: HashMap<i64, ProducerStateEntry>,
+    last_map_offset: i64,
+    last_snap_offset: i64,
+    // The ongoing txs ordere by first transaction offset
+    ongoing_txns: BTreeMap<i64, TxnMetadata>,
+
+    // completed txns whose markers are at offsets above the high watermark
+    unreplicated_txns: BTreeMap<i64, TxnMetadata>,
+}
+
+impl ProducerStateManager {
+    pub fn new(
+        topic_partition: TopicPartition,
+        log_dir: PathBuf,
+        max_producer_id_expiration_ms: Option<i64>,
+    ) -> Self {
+        Self {
+            topic_partition,
+            log_dir,
+            max_producer_id_expiration_ms: max_producer_id_expiration_ms.unwrap_or(60 * 60 * 1000),
+            log_ident: format!(
+                "[ProducerStateManager partition=${}] ",
+                topic_partition.to_string()
+            ),
+            producers: HashMap::new(),
+            last_map_offset: 0,
+            last_snap_offset: 0,
+            ongoing_txns: BTreeMap::new(),
+        }
+    }
+}
