30b45476148c3d331c88bc49030d08507151e3b3
Seb Ospina <kraige@gmail.com>
2022-05-08 12:41:47 +0200

    Init EpochEntry

--- rafka_core/src/server/epoch/leader_epoch_file_cache.rs
+++ rafka_core/src/server/epoch/leader_epoch_file_cache.rs
@@ -1,5 +1,18 @@
 //! From kafka/server/epoch/LeaderEpochFileCache.scala
-//!
 
-pub struct LeaderEpochFileCache{
+use core::fmt;
+
+#[derive(Debug)]
+pub struct LeaderEpochFileCache {}
+
+// Mapping of epoch to the first offset of the subsequent epoch
+pub struct EpochEntry {
+    epoch: i32,
+    start_offset: i64,
+}
+
+impl fmt::Display for EpochEntry {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "EpochEntry(epoch={}, startOffset={})", self.epoch, self.start_offset)
+    }
 }
