67451965d60883e157c2b4c8214461eaade58e76
Seb Ospina <kraige@gmail.com>
2022-05-23 21:07:42 +0200

    Adding basic fields and is_empty

--- rafka_core/src/server/epoch/leader_epoch_file_cache.rs
+++ rafka_core/src/server/epoch/leader_epoch_file_cache.rs
@@ -2,10 +2,43 @@
 
 use core::fmt;
 
+use crate::common::topic_partition::TopicPartition;
+use crate::server::checkpoints::leader_epoch_checkpoint_file::{
+    LeaderEpochCheckpoint, LeaderEpochCheckpointFile,
+};
+
 #[derive(Debug)]
-pub struct LeaderEpochFileCache {}
+pub struct LeaderEpochFileCache {
+    topic_partition: TopicPartition,
+    // This used to be `() => Long`, a function that returns the offset.
+    log_end_offset: i64,
+    checkpoint: LeaderEpochCheckpointFile,
+    epochs: Vec<EpochEntry>,
+    log_ident: String,
+}
+
+impl LeaderEpochFileCache {
+    pub fn new(
+        topic_partition: TopicPartition,
+        log_end_offset: i64,
+        checkpoint: LeaderEpochCheckpointFile,
+    ) -> Self {
+        Self {
+            topic_partition,
+            log_end_offset,
+            checkpoint,
+            epochs: checkpoint.read(),
+            log_ident: format!("[LeaderEpochCache {topic_partition}] "),
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.epochs.is_empty()
+    }
+}
 
 // Mapping of epoch to the first offset of the subsequent epoch
+#[derive(Debug)]
 pub struct EpochEntry {
     pub epoch: i32,
     pub start_offset: i64,
