774e918c0be096198c5dcf695efe95a452d49540
Seb Ospina <kraige@gmail.com>
2022-06-12 19:01:49 +0200

    Fixing comment and debug message

--- rafka_core/src/utils/mod.rs
+++ rafka_core/src/utils/mod.rs
@@ -5,12 +5,11 @@ pub mod verifiable_properties;
 use std::io::ErrorKind;
 use std::path::Path;
 
-/// Removes a file unless it exists.
-// RAFKA TODO: Move to a utils module
+/// Removes a file if it exists.
 pub fn delete_file_if_exists(file: &Path) -> Result<(), std::io::Error> {
     match std::fs::remove_file(file) {
         Ok(()) => {
-            tracing::debug!("Successfully deleted leader_epoch_file: {}", file.display());
+            tracing::debug!("Successfully deleted file: {}", file.display());
         },
         Err(err) => match err.kind() {
             ErrorKind::NotFound => {
