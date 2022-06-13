c9a749dabcc36fcbb77716c5574b2e10ec3af7a3
Seb Ospina <kraige@gmail.com>
2022-06-13 19:41:59 +0200

    Minor docs and change on new function

--- rafka_core/src/log/offset_index.rs
+++ rafka_core/src/log/offset_index.rs
@@ -1,7 +1,12 @@
 //! From kafka/log/OffsetIndex.scala
-//!
+//! An OffsetIndex file contains a set of 8-byte values.
+//! These 8-byte values consist of:
+//! - (4 bytes) relative offset of a message
+//! - (4 bytes) physical position inside the file on disk of such message
 
+use std::path::PathBuf;
 
+#[derive(Debug)]
 pub struct OffsetIndex {
     file: PathBuf,
     base_offset: i64,
@@ -11,13 +16,8 @@ pub struct OffsetIndex {
 }
 
 impl OffsetIndex {
-    fn new(file: PathBuf, base_offset: i64) -> Self {
-        Self{
-            file,
-            base_offset,
-            max_index_size: -1,
-            writable: true,
-            entry_size: 8
-        }
+    // RAFKA TODO: defaults are: max_index_size: -1, writable: true, entry_size: 8
+    fn new(file: PathBuf, base_offset: i64, max_index_size: i32, writable: bool) -> Self {
+        Self { file, base_offset, max_index_size, writable, entry_size: 8 }
     }
 }
