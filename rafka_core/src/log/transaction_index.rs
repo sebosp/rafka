5f0d47dd2429bcd42da6046075ab0d794a4422b7
Seb Ospina <kraige@gmail.com>
2022-06-20 22:00:54 +0200

    Adding basic docs

--- rafka_core/src/log/transaction_index.rs
+++ rafka_core/src/log/transaction_index.rs
@@ -1,4 +1,12 @@
 //! From kafka/log/TransactionIndex.scala
+//!
+//! Contains medata about aborted transactions for a segment. The metadata includes start and end
+//! offsets of such aborted transaction and the last stable offset (LSO) when the abort took place.
+//! The aborted transactions in a fetch request range at READ_COMMITTED isolation level are found
+//! Individual transactions may reference multiple segments, the recovery process needs to scan
+//! early segments to locate the start of the transaction.
+//! There is max one transaction index for a segment, its entries point to commit markers that were
+//! written in a log segment.
 
 use std::path::PathBuf;
 #[derive(Debug)]
